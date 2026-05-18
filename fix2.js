24f1a9ff50663f0df1342b2d88c7136583c89ddf
Phelefice <fleichtneto@gmail.com>
2026-05-18 15:39:39 +0200

    chore: remove temp scripts

--- fix2.js
+++
@@ -1,1 +0,0 @@
-const fs=require("fs"); let c=fs.readFileSync("app/articles/[slug]/page.tsx","utf8"); console.log(c);
--- fix3.js
+++
@@ -1,1 +0,0 @@
-const fs=require("fs"); let c=fs.readFileSync("app/articles/[slug]/page.tsx","utf8"); c=c.split("/articles").join("/"); fs.writeFileSync("app/articles/[slug]/page.tsx",c,"utf8"); console.log("OK");
