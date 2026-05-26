635590405a2b2a02b765f138cb0fed7a214eaf47
Fernando Leicht <fleichtneto@gmail.com>
2026-05-26 11:54:00 +0200

    chore: remove temp script

--- fix-bing2.js
+++
@@ -1,8 +0,0 @@
-﻿const fs = require("fs");
-let content = fs.readFileSync("app/layout.tsx", "utf8");
-content = content.replace(
-  '<meta name="google-adsense-account" content="ca-pub-5219702409402620" />',
-  '<meta name="google-adsense-account" content="ca-pub-5219702409402620" />\n        <meta name="msvalidate.01" content="76EE311F26EF52D8045CA871A9C70822" />'
-);
-fs.writeFileSync("app/layout.tsx", content, "utf8");
-console.log("Done");
